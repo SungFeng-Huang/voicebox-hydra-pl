f1a58ef5e3caebfe66b05c24a826cf41f5d0efc7
SungFeng-Huang <f06942045@ntu.edu.tw>
2023-11-02 09:46:40 +0000

    LibriLight/LibriHeavy datamodule prepare

--- tests/test_datamodules.py
+++ tests/test_datamodules.py
@@ -4,6 +4,7 @@ import pytest
 import torch
 
 from src.data.mnist_datamodule import MNISTDataModule
+from src.data.librilight_datamodule import LibriLightDataModule
 
 
 @pytest.mark.parametrize("batch_size", [32, 128])
@@ -36,3 +37,35 @@ def test_mnist_datamodule(batch_size: int) -> None:
     assert len(y) == batch_size
     assert x.dtype == torch.float32
     assert y.dtype == torch.int64
+
+@pytest.mark.parametrize("batch_size", [4, 8])
+def test_librilight_datamodule(batch_size: int) -> None:
+    """Tests `MNISTDataModule` to verify that it can be downloaded correctly, that the necessary
+    attributes were created (e.g., the dataloader objects), and that dtypes and batch sizes
+    correctly match.
+
+    :param batch_size: Batch size of the data to be loaded by the dataloader.
+    """
+    data_dir = "/datasets/LibriLight/"
+    out_data_dir = "data/LibriHeavy/"
+
+    dm = LibriLightDataModule(data_dir=data_dir, batch_size=batch_size)
+    dm.prepare_data()
+
+    assert not dm.data_train and not dm.data_val and not dm.data_test
+    assert Path("data", "LibriHeavy").exists()
+    # assert Path(data_dir, "MNIST", "raw").exists()
+
+    # dm.setup()
+    # assert dm.data_train and dm.data_val and dm.data_test
+    # assert dm.train_dataloader() and dm.val_dataloader() and dm.test_dataloader()
+
+    # num_datapoints = len(dm.data_train) + len(dm.data_val) + len(dm.data_test)
+    # assert num_datapoints == 70_000
+
+    # batch = next(iter(dm.train_dataloader()))
+    # x, y = batch
+    # assert len(x) == batch_size
+    # assert len(y) == batch_size
+    # assert x.dtype == torch.float32
+    # assert y.dtype == torch.int64
